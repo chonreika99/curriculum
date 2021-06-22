6ea3ff54f9b58bb3621abc87ef0d399f6592556f
chonreika99 <83127070+chonreika99@users.noreply.github.com>
2021-06-22 22:16:08 +0900

    課題提出

---
+++ 2-7/try.php
@@ -0,0 +1,25 @@
+<?php
+$color = ["red" => "赤","blue" => "青","green" => "緑"];
+
+echo $color ["red"];
+echo $color ["blue"];
+echo $color ["green"];
+
+var_dump($color);
+
+?>
+
+<?php
+echo '<br>';
+$color = ["red" => "赤","blue" => "青","green" => "緑"];
+$color  ["yellow"]= "黄色";
+
+echo $color ["red"];
+echo $color ["blue"];
+echo $color ["green"];
+echo $color ["yellow"];
+
+var_dump($color);
+
+?>
+

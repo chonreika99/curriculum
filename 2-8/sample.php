5cade0a051d62aad257b6bd6363038fd509212cb
chonreika99 <83127070+chonreika99@users.noreply.github.com>
2021-06-25 10:52:51 +0900

    課題提出

---
+++ 2-8/sample.php
@@ -0,0 +1,11 @@
+<?php
+$fruits = ["apple" => "りんご","orange" => "みかん","peach" => "もも"];
+
+foreach ($fruits as $key => $value){
+    $word = ["といったら"];
+
+    echo $key;
+    echo $word[0];
+    echo $value;
+}
+?>

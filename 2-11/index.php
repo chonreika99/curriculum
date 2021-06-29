68bc4090dbec5eb6e52c5ca895d9151559e3db13
chonreika99 <83127070+chonreika99@users.noreply.github.com>
2021-06-29 22:20:13 +0900

    課題提出（フォーム）

---
+++ 2-11/index.php
@@ -0,0 +1,9 @@
+<form action="reslt.php" method="post">
+名前：<input type="text" name="my_name" />
+<br>
+メールアドレス：<input type="mailaddress" name="mailaddress"/>
+<br>
+パスワード：<input type="password" name="password" />
+<br>
+<input type="submit" value="送信" />
+</form>

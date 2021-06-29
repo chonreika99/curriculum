75acc138f2c944e31c29fd5b6f704ec4a92f0394
chonreika99 <83127070+chonreika99@users.noreply.github.com>
2021-06-29 20:25:56 +0900

    課題再提出（FizzBuzz）

--- 2-9/try.php
+++ 2-9/try.php
@@ -1,7 +1,7 @@
 <?php
 for ($num = 1;$num < 101;$num++) {
  
-    if($num % 15 == 0){
+    if($num % 3*5 == 0){
         //15で割った余りが0（＝15の倍数）のとき
         echo "FizzBuzz!!";
     }elseif($num % 3 == 0){

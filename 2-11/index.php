51a64f7c91ba1ad130ebd52d65052444be671bd4
chonreika99 <83127070+chonreika99@users.noreply.github.com>
2021-07-01 14:11:58 +0900

    Update index.php

--- 2-11/index.php
+++ 2-11/index.php
@@ -1,4 +1,4 @@
-<form action="reslt.php" method="post">
+<form action="result.php" method="post">
 名前：<input type="text" name="my_name" />
 <br>
 メールアドレス：<input type="mailaddress" name="mailaddress"/>
